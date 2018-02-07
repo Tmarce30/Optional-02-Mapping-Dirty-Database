fb038de23884e39e58cbff71b0fcd54d782e5fa1
Thibaud Marcé <thibaud.marce@gmail.com>
2018-02-07 12:38:55 +0100

    chenged foreign key name for id_artist

--- app/models/album.rb
+++ app/models/album.rb
@@ -2,7 +2,7 @@ class Album < ActiveRecord::Base
   self.table_name = "THE_ALBUMS"
 
   has_many :tracks
-  belongs_to :artist
+  belongs_to :artist, foreign_key: 'id_artist'
 
   validates :title, presence: true
   validates :id_artist, presence: true
